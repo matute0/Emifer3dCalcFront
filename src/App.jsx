5d97e3686b262f8c576279b77b44fa9a71644020
matute0 <matifernandezescuder@hotmail.com>
2026-09-01 23:08:30 -0300

    feat: implement PrintersManager component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 import Login from './pages/Login';
 import Admin from './pages/Admin';
 import ProtectedRoute from './middleware/ProtectedRoute';
-import Printers from './pages/Printers';
 import Filaments from './pages/Filament';
 import Costs from './pages/Costs';
+import PrintersManager from './pages/Printers';
 
 export default function App() {
   const location = useLocation();
@@ -15,7 +15,7 @@ export default function App() {
       <Route path="/" element={<InitialPage/>} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/admin" element={<ProtectedRoute element={<Admin/>}/>}/>
-      <Route path="/admin/printers" element={<ProtectedRoute element={<Printers/>}/>}/>
+      <Route path="/admin/printers" element={<ProtectedRoute element={<PrintersManager/>}/>}/>
       <Route path="/admin/filaments" element={<ProtectedRoute element={<Filaments/>}/>}/>
       <Route path="/admin/costs" element={<ProtectedRoute element={<Costs/>}/>}/>
     </Routes>
