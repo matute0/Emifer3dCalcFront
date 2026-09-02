0889fcbe3eef922985603bd4fdab4c83ff5c0d17
matute0 <matifernandezescuder@hotmail.com>
2026-09-01 22:22:47 -0300

    feat: add routes and components for Printers, Filaments, and Costs in admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Login from './pages/Login';
 import Admin from './pages/Admin';
 import ProtectedRoute from './middleware/ProtectedRoute';
+import Printers from './pages/Printers';
+import Filaments from './pages/Filament';
+import Costs from './pages/Costs';
 
 export default function App() {
   const location = useLocation();
@@ -12,6 +15,9 @@ export default function App() {
       <Route path="/" element={<InitialPage/>} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/admin" element={<ProtectedRoute element={<Admin/>}/>}/>
+      <Route path="/admin/printers" element={<ProtectedRoute element={<Printers/>}/>}/>
+      <Route path="/admin/filaments" element={<ProtectedRoute element={<Filaments/>}/>}/>
+      <Route path="/admin/costs" element={<ProtectedRoute element={<Costs/>}/>}/>
     </Routes>
   )
 }
